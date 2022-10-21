d5a2e62a769cc3c4f2688e95a84c0485d710c082
chands <chands1432@gmail.com>
2022-10-21 16:50:38 +0530

    basic button toggling options added,Completed basic version of ToDo List

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ let btnClear = $('#btnClear');
 let ulTasks = $('#ulTasks');
 
 //buttons functionality
-btnAdd.click(addItem);
-
 function addItem() {
     // create new list item
     let listItem = $('<li>', {
@@ -20,7 +18,9 @@ function addItem() {
         listItem.toggleClass('done');
         //since listItem is in the closure so use directly.
         //still need to make the clicked listItem clickable again so that we can toggle it again.
-        //"pointer-events:none, came due to bootstrap 'disabled' class" must be removed to change the class instead to 'done' or anthing else.
+        //"pointer-events:none", came due to bootstrap 'disabled'
+        //class must be removed to change the class instead to 'done' or anthing else.
+        toggleClearSortBtn();//use when listItem's done class is toggled.
     })
     ulTasks.append(listItem);
     inpNewTask.val(""); //clear input field
@@ -37,14 +37,41 @@ function sortTasks() {
     $('#ulTasks .done').appendTo(ulTasks);
 }
 
+//toggle buttons disabled when we needn't to use it.
+function toggleInputBtn() {
+    btnReset.prop('disabled', inpNewTask.val() == '');
+    btnAdd.prop('disabled', inpNewTask.val() == '');
+}
+
+function toggleClearSortBtn() {
+    btnSort.prop('disabled', $('#ulTasks .done').length == 0);
+    btnClear.prop('disabled', $('#ulTasks .done').length == 0);
+}
+
 //pressing "Enter" key add Task in the list:
 inpNewTask.keypress((e) => {
     //find which is is being pressed. 'Enter'=13(Event.which value)
     //console.log(e.which);
     if (e.which == 13) addItem();
+    toggleInputBtn();
 })
 
-btnReset.click(() => inpNewTask.val(""))
+inpNewTask.on('input', () => {
+    //console.log(inpNewTask.val());//when its on enable reset btn else disable.
+    toggleInputBtn();
+})
 
-btnClear.click(clearFinishedTasks);
+btnAdd.click(() => {
+    addItem();
+    toggleInputBtn();
+});
+btnReset.click(() => {
+    inpNewTask.val("");
+    toggleInputBtn();
+});
 btnSort.click(sortTasks);
+btnClear.click(() => {
+    let isClear = confirm(`This will delete your completed Tasks!
+    Do you want to delete it?`);
+    if (isClear) clearFinishedTasks();
+});
